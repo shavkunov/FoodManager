2fb38b0aca67e0f087d3e3e32eec8275d2f7bbad
shavkunov <mv.shavkunov@yandex.ru>
2016-11-11 00:55:43 +0300

    implemented MenuStorage Skeleton

--- app/src/main/java/ru/spbau/mit/foodmanager/model/MenuStorage.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/model/MenuStorage.java
@@ -1,6 +1,7 @@
 package ru.spbau.mit.foodmanager.model;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 enum Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
 
@@ -8,18 +9,96 @@ enum Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
  * Реализация хранилища меню.
  */
 public class MenuStorage {
-    public class Menu {
-        private List<List<Recipe>> listDishes;
+    /**
+     * Список рецептов на неделю.
+     */
+    private ArrayList<ArrayList<Recipe>> listDishes;
+
+    MenuStorage() {
+        listDishes = new ArrayList<>();
+        // TODO load from DataBase
     }
 
-    public Menu getDayMenu(Day day) {
-        return null;
+    /**
+     * Создание меню для одного дня.
+     * Гарантируется, что рецепты не будут совпадать с уже выбранными.
+     * Если такая выборка невозможна(если рецептов будет много, то такой проблемы не возникнет)
+     * то можно в дальнейшем реализовать с повторениями.
+     * @param day день в который нужно сгенерировать меню
+     */
+    private void generateDayMenu(Day day) {
+        HashMap<Recipe, Integer> m = new HashMap<>();
+
+        for (int i = 0; i < listDishes.size(); i++) {
+            ArrayList<Recipe> curDay = listDishes.get(i);
+
+            if (curDay != null) {
+                for (int j = 0; j < curDay.size(); j++) {
+                    Recipe curRecipe = curDay.get(j);
+                    m.put(curRecipe, 1);
+                }
+            }
+        }
+
+        Recipe breakfast = Category.chooseRandomBreakfast();
+        while (m.get(breakfast) != null) {
+            breakfast = Category.chooseRandomBreakfast();
+        }
+
+        Recipe lunch = Category.chooseRandomLunch();
+        while (m.get(breakfast) != null) {
+            lunch = Category.chooseRandomLunch();
+        }
+
+        Recipe dinner = Category.chooseRandomDinner();
+        while (m.get(breakfast) != null) {
+            dinner = Category.chooseRandomDinner();
+        }
+
+        ArrayList<Recipe> dayMenu = new ArrayList<>();
+        dayMenu.add(breakfast);
+        dayMenu.add(lunch);
+        dayMenu.add(dinner);
+
+        listDishes.set(day.ordinal(), dayMenu);
     }
 
-    public void setDayMenu(Day day, Menu menu) {
+    /**
+     * @return возвращает меню. i-ая запись в списке соответствует списку рецептов на i-ый день.
+     * Внимание: запись в списке может означать null, это значит, что нет меню на этот день.
+     * Сделано это для того, чтобы мы могли создавать меню, например, только на один день.
+     */
+    public ArrayList<ArrayList<Recipe>> getMenu() {
+        return listDishes;
     }
 
-    public List<Recipe> getDayMenuList() {
-        return null;
+    /**
+     * Используется ТОЛЬКО в случае, если пользователя не устраивает конкретный день в меню.
+     * @param day метод заново создает меню для этого дня.
+     */
+    public void setNewDayMenu(Day day) {
+        generateDayMenu(day);
+    }
+
+    /**
+     * Создание меню на неделю.
+     */
+    public void setNewWeekMenu() {
+        for (Day day : Day.values()) {
+            generateDayMenu(day);
+        }
+    }
+
+    /**
+     * @param day создание меню только для одного дня на неделе.
+     */
+    public void setOnlyOneDayMenu(Day day) {
+        for (Day d : Day.values()) {
+            if (d == day) {
+                generateDayMenu(d);
+            } else {
+                listDishes.set(d.ordinal(), null);
+            }
+        }
     }
 }
