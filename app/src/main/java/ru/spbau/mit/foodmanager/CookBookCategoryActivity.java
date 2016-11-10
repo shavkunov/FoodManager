b068141786c48a299f690a2d2d9b5da9a7aab973
ArgentumWalker <svidchen-ko@amil.ru>
2016-11-10 12:58:40 +0300

    Basic CookBookCategory activity

---
+++ app/src/main/java/ru/spbau/mit/foodmanager/CookBookCategoryActivity.java
@@ -0,0 +1,13 @@
+package ru.spbau.mit.foodmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CookBookCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cook_book_category);
+    }
+}
