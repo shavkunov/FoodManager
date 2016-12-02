2f4bf07a6d8ac916cc4cf5227315113641d6b0d5
ArgentumWalker <svidchen-ko@mail.ru>
2016-12-02 23:40:50 +0300

    RecipeView step images at bottom

--- app/src/main/java/ru/spbau/mit/foodmanager/RecipeViewActivity.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/RecipeViewActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +17,8 @@ import java.util.ArrayList;
 public class RecipeViewActivity extends AppCompatActivity {
     private static final String INGRIDIENT_LIST_DIVIDER = " - ";
     private static final String INGRIDIENT_LIST_COUNT_DIVIDER = "        ";
+    private static final int IMAGES_WIDTH = 200; //dp
+    private static final int IMAGES_HEIGHT = 150; //dp
     private static Recipe recipe;
 
     @Override
@@ -24,10 +28,12 @@ public class RecipeViewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         CookBookStorage cookbook = new CookBookStorage(this);
         recipe = cookbook.getRecipe(intent.getIntExtra("Recipe", -1));
+        ArrayList<Step> steps = recipe.getStepByStep();
         //Header
         TextView nameView = (TextView)findViewById(R.id.recipe_header_name);
         ImageView photoView = (ImageView)findViewById(R.id.recipe_header_photo);
         LinearLayout categoryList = (LinearLayout)findViewById(R.id.recipe_header_tags);
+        photoView.setImageBitmap(steps.get(steps.size()-1).getImage());
         for (final Integer id : recipe.getCategoryID()) {
             Button categoryNameView = new Button(this);
             //TextView categoryNameView = new TextView(this);
@@ -48,6 +54,16 @@ public class RecipeViewActivity extends AppCompatActivity {
         //Body
         TextView descriptionView = (TextView)findViewById(R.id.recipe_body_description);
         TextView ingridientsView = (TextView)findViewById(R.id.recipe_body_ingredients);
+        LinearLayout recipeImages = (LinearLayout)findViewById(R.id.recipe_body_images);
+        int widthInPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, IMAGES_WIDTH, getResources().getDisplayMetrics());
+        int heightInPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, IMAGES_HEIGHT, getResources().getDisplayMetrics());
+        for (Step step : steps) {
+            ImageView photo = new ImageView(this);
+            photo.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            photo.setLayoutParams(new LinearLayout.LayoutParams(widthInPx, heightInPx));
+            photo.setImageBitmap(step.getImage());
+            recipeImages.addView(photo);
+        }
 
         nameView.setText(recipe.getName());
         descriptionView.setText(recipe.getDescription());
@@ -64,8 +80,7 @@ public class RecipeViewActivity extends AppCompatActivity {
             }
         }
         ingridientsView.setText(ingridientList);
-        ArrayList<Step> steps = recipe.getStepByStep();
-        photoView.setImageBitmap(steps.get(steps.size()-1).getImage());
+
     }
 
     public void onCookClick(View v) {
