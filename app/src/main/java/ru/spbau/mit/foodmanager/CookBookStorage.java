158b2e0a4b3d27937d0fda2fc2323eae4511ebf3
shavkunov <mv.shavkunov@yandex.ru>
2016-12-04 22:11:01 +0300

    simplified conditions in CookbookStorage

--- app/src/main/java/ru/spbau/mit/foodmanager/CookBookStorage.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/CookBookStorage.java
@@ -47,17 +47,13 @@ public class CookBookStorage {
                                      null, null, null);
 
         ArrayList<Integer> ids = new ArrayList<>();
-        if (categories != null) {
-            if (categories.moveToFirst()) {
-                do {
-                    int categoryID = categories.getInt(categories.getColumnIndex("category_ID"));
-                    ids.add(categoryID);
+        if (categories != null && categories.moveToFirst()) {
+            do {
+                int categoryID = categories.getInt(categories.getColumnIndex("category_ID"));
+                ids.add(categoryID);
 
-                } while (categories.moveToNext());
+            } while (categories.moveToNext());
 
-            } else {
-                return null;
-            }
         } else {
             return null;
         }
@@ -77,21 +73,17 @@ public class CookBookStorage {
         Cursor steps = db.rawQuery(stepsQuery, new String[] {String.valueOf(ID)});
 
         ArrayList<Step> recipeSteps = new ArrayList<>();
-        if (steps != null) {
-            if (steps.moveToFirst()) {
-                do {
+        if (steps != null && steps.moveToFirst()) {
+            do {
 
-                    String description = steps.getString(steps.getColumnIndex("description"));
-                    byte[] bytes = steps.getBlob(steps.getColumnIndex("source"));
-                    Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                    Step newStep = new Step(description, bm);
-                    recipeSteps.add(newStep);
+                String description = steps.getString(steps.getColumnIndex("description"));
+                byte[] bytes = steps.getBlob(steps.getColumnIndex("source"));
+                Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                Step newStep = new Step(description, bm);
+                recipeSteps.add(newStep);
 
-                } while (steps.moveToNext());
+            } while (steps.moveToNext());
 
-            } else {
-                return null;
-            }
         } else {
             return null;
         }
@@ -112,21 +104,17 @@ public class CookBookStorage {
         Cursor ingredients = db.rawQuery(ingredientsQuery, new String[] {String.valueOf(ID)});
 
         ArrayList<Ingredient> recipeIngredients = new ArrayList<>();
-        if (ingredients != null) {
-            if (ingredients.moveToFirst()) {
-                do {
-                    String name = ingredients.getString(ingredients.getColumnIndex("name"));
-                    Measure measure = Measure.values()[
-                            ingredients.getInt(ingredients.getColumnIndex("measure"))];
-                    double quantity = ingredients.getDouble(ingredients.getColumnIndex("quantity"));
+        if (ingredients != null && ingredients.moveToFirst()) {
+            do {
+                String name = ingredients.getString(ingredients.getColumnIndex("name"));
+                Measure measure = Measure.values()[
+                        ingredients.getInt(ingredients.getColumnIndex("measure"))];
+                double quantity = ingredients.getDouble(ingredients.getColumnIndex("quantity"));
 
-                    Ingredient ingredient = new Ingredient(name, measure, quantity);
-                    recipeIngredients.add(ingredient);
-                } while (ingredients.moveToNext());
+                Ingredient ingredient = new Ingredient(name, measure, quantity);
+                recipeIngredients.add(ingredient);
+            } while (ingredients.moveToNext());
 
-            } else {
-                return null;
-            }
         } else {
             return null;
         }
@@ -147,15 +135,11 @@ public class CookBookStorage {
         Cursor mainData = db.query("Recipe", new String[] {"name", "description"},
                 "ID = ?", new String[] { String.valueOf(ID) }, null, null, null);
 
-        if (mainData != null) {
-            if (mainData.moveToFirst()) {
-                String recipeName = mainData.getString(mainData.getColumnIndex("name"));
-                String recipeDescription = mainData.getString(mainData.getColumnIndex("description"));
+        if (mainData != null && mainData.moveToFirst()) {
+            String recipeName = mainData.getString(mainData.getColumnIndex("name"));
+            String recipeDescription = mainData.getString(mainData.getColumnIndex("description"));
 
-                res = new Recipe(ID, recipeDescription, recipeName);
-            } else {
-                return null;
-            }
+            res = new Recipe(ID, recipeDescription, recipeName);
         } else {
             return null;
         }
@@ -172,16 +156,12 @@ public class CookBookStorage {
         Cursor recipes = db.rawQuery(filterQuery, null);
 
         ArrayList<Recipe> res = new ArrayList<>();
-        if (recipes != null) {
-            if (recipes.moveToFirst()) {
-                do {
-                    int recipeID = recipes.getInt(recipes.getColumnIndex("ID"));
-                    res.add(getRecipe(recipeID));
-                } while (recipes.moveToNext());
+        if (recipes != null && recipes.moveToFirst()) {
+            do {
+                int recipeID = recipes.getInt(recipes.getColumnIndex("ID"));
+                res.add(getRecipe(recipeID));
+            } while (recipes.moveToNext());
 
-            } else {
-                return null;
-            }
         } else {
             return null;
         }
@@ -204,16 +184,12 @@ public class CookBookStorage {
         String categoryQuery = "SELECT * FROM Recipe_to_category WHERE category_ID = " + ID;
         Cursor cursor = db.rawQuery(categoryQuery, null);
 
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                do {
-                    int recipeID = cursor.getInt(cursor.getColumnIndex("recipe_ID"));
-                    res.add(getRecipe(recipeID));
-                } while (cursor.moveToNext());
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                int recipeID = cursor.getInt(cursor.getColumnIndex("recipe_ID"));
+                res.add(getRecipe(recipeID));
+            } while (cursor.moveToNext());
 
-            } else {
-                return null;
-            }
         } else {
             return null;
         }
@@ -227,18 +203,15 @@ public class CookBookStorage {
         String categoryQuery = "SELECT * FROM Category WHERE ID = " + ID;
         Cursor categoryCursor = db.rawQuery(categoryQuery, null);
 
-        if (categoryCursor != null) {
-            if (categoryCursor.moveToFirst()) {
-                String description = categoryCursor.getString(
-                                     categoryCursor.getColumnIndex("name"));
+        if (categoryCursor != null && categoryCursor.moveToFirst()) {
+            String description = categoryCursor.getString(
+                                 categoryCursor.getColumnIndex("name"));
 
-                c = new Category(ID, description, this, null); // пока картинок категорий у нас нет
-            } else {
-                return null;
-            }
+            c = new Category(ID, description, this, null); // пока картинок категорий у нас нет
         } else {
             return null;
         }
+        
         categoryCursor.close();
         return c;
     }
