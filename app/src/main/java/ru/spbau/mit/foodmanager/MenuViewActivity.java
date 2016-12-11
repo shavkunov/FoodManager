7c64c9fd4ffe9315069d2ecb81cd4114eccd8101
ArgentumWalker <svidchen-ko@mail.ru>
2016-12-11 18:51:13 +0300

    Set recipes in MenuView Clickable

--- app/src/main/java/ru/spbau/mit/foodmanager/MenuViewActivity.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/MenuViewActivity.java
@@ -1,37 +1,32 @@
 package ru.spbau.mit.foodmanager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MenuViewActivity extends AppCompatActivity {
     private HashMap<Day, DayMenu> allDayMenu;
     private CookBookStorage cookbook;
     private MenuStorage menu;
+    private LayoutInflater inflater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_view);
+
+        inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         cookbook = CookBookStorage.getInstance(this);
         menu = MenuStorage.getInstance(this);
         allDayMenu = menu.getMenu();
         showRecipes();
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
     }
 
     public void onGenerateBtnClick(View v) {
@@ -41,8 +36,57 @@ public class MenuViewActivity extends AppCompatActivity {
     }
 
     private void showRecipes() {
-        ListView daysListView = (ListView) findViewById(R.id.menu_view_list_view);
-        BaseAdapter adapter = new MenuViewAdapter(this, allDayMenu);
-        daysListView.setAdapter(adapter);
+        LinearLayout daysListView = (LinearLayout) findViewById(R.id.menu_view_days);
+        daysListView.removeAllViews();
+        for (Day d : Day.values()) {
+            generateDayMenuView(d, daysListView);
+        }
+    }
+
+    private void generateDayMenuView(Day day, LinearLayout parent) {
+        View view = inflater.inflate(R.layout.menu_view_list_element, null);
+        DayMenu menu = allDayMenu.get(day);
+        TextView dayName = (TextView) view.findViewById(R.id.menu_day_name);
+        LinearLayout dayMealtimes = (LinearLayout) view.findViewById(R.id.menu_day_mealtimes);
+
+        if (menu != null) {
+            dayName.setText(MenuStorage.getDayNames()[day.ordinal()]);
+            for (Integer i = 0; i < menu.getMealtimes().size(); i++) {
+                generateMealtimeView(menu, i, dayMealtimes);
+            }
+            parent.addView(view);
+        }
+    }
+
+    private void generateMealtimeView(DayMenu menu, Integer position, LinearLayout parent) {
+        LinearLayout mealtimeElement = (LinearLayout) inflater.inflate(
+                R.layout.menu_view_day_menu_list_element, null);
+        TextView mealtimeName = (TextView) mealtimeElement.findViewById(
+                R.id.menu_day_mealtime_name);
+        LinearLayout recipeList = (LinearLayout) mealtimeElement.findViewById(
+                R.id.menu_day_mealtime_dishes);
+        DayMenu.Mealtime mealtime = menu.getMealtimes().get(position);
+
+        mealtimeName.setText(mealtime.getName());
+        for (Integer i = 0; i < mealtime.getRecipes().size(); i++) {
+            generateRecipeView(mealtime, i, recipeList);
+        }
+        parent.addView(mealtimeElement);
+    }
+
+    private void generateRecipeView(DayMenu.Mealtime mealtime, Integer position, LinearLayout parent) {
+        TextView recipeName = new TextView(this);
+        final Integer recipeID = mealtime.getRecipes().get(position);
+
+        recipeName.setText(cookbook.getRecipe(recipeID).getName());
+        recipeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuViewActivity.this, RecipeViewActivity.class);
+                intent.putExtra("Recipe", recipeID);
+                startActivity(intent);
+            }
+        });
+        parent.addView(recipeName);
     }
 }
