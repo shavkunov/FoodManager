3daab23ee64dce3fc0c2b0e71fecfe3a6853d1f3
ArgentumWalker <svidchen-ko@mail.ru>
2016-12-11 13:13:47 +0300

    Calling CookBookActivity from MealtimeSettingsActivity

--- app/src/main/java/ru/spbau/mit/foodmanager/MealtimeSettingsActivity.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/MealtimeSettingsActivity.java
@@ -12,11 +12,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MealtimeSettingsActivity extends AppCompatActivity {
+    private static final int REQUEST_ADD = 0;
+    private static final int REQUEST_EDIT = 1;
     private CookBookStorage cookbook;
     private ArrayList<Integer> categories = new ArrayList<>();
+    private HashMap<Integer, View> views = new HashMap<>();
     private LayoutInflater inflater;
+    private LinearLayout dishes;
     private Intent task;
 
     @Override
@@ -26,13 +31,13 @@ public class MealtimeSettingsActivity extends AppCompatActivity {
 
         cookbook = CookBookStorage.getInstance(this);
         inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        dishes = (LinearLayout) findViewById(R.id.mealtime_settings_dishes);
         task = getIntent();
         DaySettings.MealtimeSettings settings = (DaySettings.MealtimeSettings)
                 task.getSerializableExtra("Settings");
 
         if (settings != null) {
             EditText name = (EditText) findViewById(R.id.mealtime_settings_name);
-            LinearLayout dishes = (LinearLayout) findViewById(R.id.mealtime_settings_dishes);
             categories.addAll(settings.dishesCategories());
 
             name.setText(settings.getName());
@@ -45,9 +50,32 @@ public class MealtimeSettingsActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO call category choose
+                Intent intent = new Intent(MealtimeSettingsActivity.this, CookBookActivity.class);
+                intent.putExtra("Target", CookBookActivity.TARGET_CATEGORY);
+                startActivityForResult(intent, REQUEST_ADD);
             }
         });
+    }
+
+    public void onActivityResult(int requestCode, int errorCode, Intent resultContainer) {
+        switch (requestCode) {
+            case REQUEST_ADD :
+                if (errorCode == RESULT_OK) {
+                    Integer result = resultContainer.getIntExtra("Result", -1);
+                    categories.add(result);
+                    dishes.addView(generateDishView(dishes, result));
+                }
+                break;
+            case REQUEST_EDIT :
+                if (errorCode == RESULT_OK) {
+                    Integer result = resultContainer.getIntExtra("Result", -1);
+                    Integer position = resultContainer.getIntExtra("Position", -1);
+                    categories.set(position, result);
+                    TextView categoryName = (TextView) views.get(position).findViewById(R.id.mealtime_settings_dish_name);
+                    categoryName.setText(cookbook.getCategoryByID(result).getDescription());
+                }
+                break;
+        }
     }
 
     private View generateDishView(final LinearLayout parent, final Integer id) {
@@ -56,12 +84,16 @@ public class MealtimeSettingsActivity extends AppCompatActivity {
         ImageButton editBtn = (ImageButton) dish.findViewById(R.id.mealtime_settings_dish_edit);
         ImageButton delBtn = (ImageButton) dish.findViewById(R.id.mealtime_settings_dish_delete);
         Category category = cookbook.getCategoryByID(id);
+        final Integer position = categories.indexOf(id); //TODO fix possible problems
 
         dishName.setText(category.getDescription());
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO call category choose
+                Intent intent = new Intent(MealtimeSettingsActivity.this, CookBookActivity.class);
+                intent.putExtra("Target", CookBookActivity.TARGET_CATEGORY);
+                intent.putExtra("Position", position);
+                startActivityForResult(intent, REQUEST_EDIT);
             }
         });
         delBtn.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +103,7 @@ public class MealtimeSettingsActivity extends AppCompatActivity {
                 parent.removeView(dish);
             }
         });
+        views.put(position, dish);
         return dish;
     }
 
