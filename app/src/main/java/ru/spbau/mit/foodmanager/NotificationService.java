a6c67f4b8e74673193ef446ecbfb2b83e5218409
ArgentumWalker <svidchen-ko@mail.ru>
2016-12-14 18:17:13 +0300

    Minor changes

--- app/src/main/java/ru/spbau/mit/foodmanager/NotificationService.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/NotificationService.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Сервис нотифицирования пользователя
@@ -16,6 +19,8 @@ public class NotificationService extends Service {
     private static final String COOK_NOTIFICATION_TITLE = "Время начинать готовить";
     private static final String COOK_NOTIFICATION_TEXT = "Сейчас самое время приготовить что-нибудь вкусное!";
     private NotificationManager notificationManager;
+    private Day lastCookDayNotify;
+    private Boolean notifyedToday;
     private Calendar calendar = Calendar.getInstance();
     private MenuSettings menuSettings = MenuSettings.getInstance();
     private NotificationSettings notificationSettings = NotificationSettings.getInstance();
@@ -28,17 +33,22 @@ public class NotificationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        try {
-            while (true) {
-                if (notificationSettings.getShowCookNotifications()) {
-                    sendCookNotification();
+        while (true) {
+            try {
+                while (true) {
+                    TimeUnit.SECONDS.sleep(1);
+                    if (calendarDayToDay(calendar.get(Calendar.DAY_OF_WEEK)) != lastCookDayNotify) {
+                        notifyedToday = false;
+                    }
+                    if (notificationSettings.getShowCookNotifications() && !notifyedToday) {
+                        sendCookNotification();
+                    }
                 }
+            } catch (Exception e) {
+                //Finish Service //Nope
             }
         }
-        catch (Exception e) {
-            //Finish Service
-        }
-        return super.onStartCommand(intent, flags, startId);
+        //return super.onStartCommand(intent, flags, startId);
     }
 
     /**
@@ -49,7 +59,9 @@ public class NotificationService extends Service {
         long notificationTimeBegin = notificationSettings.getTimeOfDayBeginCookNotifications();
         long notificationTimeEnd = notificationSettings.getTimeOfDayEndCookNotifications();
         Day day = calendarDayToDay(calendar.get(Calendar.DAY_OF_WEEK));
-        if (timeOfDay > notificationTimeBegin && timeOfDay < notificationTimeEnd && menuSettings.isCookingDay(day)) {
+        if (timeOfDay > notificationTimeBegin &&
+                timeOfDay < notificationTimeEnd &&
+                menuSettings.isCookingDay(day)) {
             Intent intent = new Intent(this, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, COOK_NOTIFICATION_ID, intent, 0);
             NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
@@ -58,6 +70,8 @@ public class NotificationService extends Service {
             notificationBuilder.setContentText(COOK_NOTIFICATION_TEXT);
             notificationBuilder.setContentIntent(pendingIntent);
             notificationManager.notify(COOK_NOTIFICATION_ID, notificationBuilder.build());
+            lastCookDayNotify = day; //TODO делать нормальную проверку, т.к. если только один день  готовки, то все плохо.
+            notifyedToday = true;
         }
     }
 
