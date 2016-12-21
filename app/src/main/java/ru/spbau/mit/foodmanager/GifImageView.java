39eb95c012f3ec000a0b06a276826aa5032a6bdc
ArgentumWalker <svidchen-ko@mail.ru>
2016-12-21 18:50:10 +0300

    Make GifView resizable

--- app/src/main/java/ru/spbau/mit/foodmanager/GifImageView.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/GifImageView.java
@@ -1,9 +1,11 @@
 package ru.spbau.mit.foodmanager;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Movie;
+import android.graphics.Rect;
 import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -21,9 +23,14 @@ public class GifImageView extends View {
     private InputStream mInputStream;
     private Movie mMovie;
     private int mWidth, mHeight;
+    private int realWidth = -1;
+    private int realHeight = -1;
     private long mStart;
     private Context mContext;
     private Boolean visible = true;
+    private Bitmap __bitmap;
+    private Canvas __canvas;
+    private Rect __dst;
 
     public GifImageView(Context context) {
         super(context);
@@ -47,8 +54,23 @@ public class GifImageView extends View {
     private void init() {
         setFocusable(true);
         mMovie = Movie.decodeStream(mInputStream);
-        mWidth = mMovie.width();
-        mHeight = mMovie.height();
+        __bitmap = Bitmap.createBitmap(mMovie.width(), mMovie.height(), Bitmap.Config.ARGB_8888);
+        __canvas = new Canvas(__bitmap);
+        initSize();
+    }
+
+    private void initSize() {
+        if (realWidth > 0) {
+            mWidth = realWidth;
+        } else {
+            mWidth = mMovie.width();
+        }
+        if (realHeight > 0) {
+            mHeight = realHeight;
+        } else {
+            mHeight = mMovie.height();
+        }
+        __dst = new Rect(0, 0, mWidth, mHeight);
     }
 
     @Override
@@ -70,11 +92,15 @@ public class GifImageView extends View {
             if (mMovie != null) {
                 int duration = mMovie.duration();
                 if (duration == 0) {
-                    duration = 1000;
+                    duration = 1;
                 }
                 int relTime = (int) ((now - mStart) % duration);
                 mMovie.setTime(relTime);
-                mMovie.draw(canvas, 0, 0);
+                //Draw in bitmap
+                __canvas.drawColor(Color.TRANSPARENT);
+                mMovie.draw(__canvas, 0, 0);
+                //Draw bitmap on canvas with resize
+                canvas.drawBitmap(__bitmap, null, __dst, null);
                 this.invalidate();
             }
         }
@@ -87,5 +113,21 @@ public class GifImageView extends View {
     public void setGifImageResource(int id) {
         mInputStream = mContext.getResources().openRawResource(id);
         init();
+    }
+
+    /**
+     * Set Width in pixels
+     */
+    public void setGifWidth(int w) {
+        realWidth = w;
+        initSize();
+    }
+
+    /**
+     * Set Height in pixels
+     */
+    public void setGifHeight(int h) {
+        realHeight = h;
+        initSize();
     }
 }
