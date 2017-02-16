e9e5ece9fccd0bcc506dda8abcb0f75bab7ccae0
ArgentumWalker <svidchen-ko@mail.ru>
2017-02-16 19:47:14 +0300

    bugfix

--- app/src/main/java/ru/spbau/mit/foodmanager/NotificationSettingsActivity.java
+++ app/src/main/java/ru/spbau/mit/foodmanager/NotificationSettingsActivity.java
@@ -32,6 +32,7 @@ public class NotificationSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
+        super.onDestroy();
         NotificationSettings.saveNotificationSettings(this);
     }
 
